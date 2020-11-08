6420d711837ed4f51215bab3fcdabff3672cbf62
Shopeyin Wale <shopeyinwale@gmail.com>
2020-11-08 19:39:40 +0100

    Updates

---
+++ precache-manifest.175771d2df11205b1e87507377e6cd1c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "446be2d4fa35dc1951fd403751f7e616",
+    "url": "/item7-firebase/index.html"
+  },
+  {
+    "revision": "54aab67e29d2657e88aa",
+    "url": "/item7-firebase/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "f44b21138efc89f4b215",
+    "url": "/item7-firebase/static/js/2.565ea98c.chunk.js"
+  },
+  {
+    "revision": "b01cf20ee08477db845d02c3b279da13",
+    "url": "/item7-firebase/static/js/2.565ea98c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54aab67e29d2657e88aa",
+    "url": "/item7-firebase/static/js/main.04a7cb80.chunk.js"
+  },
+  {
+    "revision": "b4a5a4f8b9b784dae2e7",
+    "url": "/item7-firebase/static/js/runtime-main.707aebde.js"
+  }
+]);
